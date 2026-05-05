3dbce1fe3742e474780da1c8cab8c513d25adf25
ppguz <ppguzmanmayen@gmail.com>
2026-05-04 22:36:43 -0600

    Feat: endpoint to send individual messages created succesfully

--- init.sql
+++ init.sql
@@ -1,38 +1,40 @@
 CREATE TABLE IF NOT EXISTS users (
-    id                    SERIAL PRIMARY KEY,
-    name                  VARCHAR(100) NOT NULL,
-    email                 VARCHAR(255) NOT NULL UNIQUE,
-    contrasenas           TEXT NOT NULL,
-    public_key            TEXT NOT NULL,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    contrasenas TEXT NOT NULL,
+    public_key TEXT NOT NULL,
     encrypted_private_key TEXT NOT NULL,
-    created_at            TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
+
+CREATE TABLE IF NOT EXISTS groups (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+
 CREATE TABLE IF NOT EXISTS messages (
-    id             SERIAL PRIMARY KEY,
-    sender_id      INT         NOT NULL REFERENCES users(id),
-    recipient_id   INT                  REFERENCES users(id),
-    group_id       INT                  REFERENCES groups(id),
-    ciphertext     TEXT        NOT NULL,
-    encrypted_key  TEXT        NOT NULL,
-    nonce          VARCHAR(24) NOT NULL,
-    auth_tag       VARCHAR(24) NOT NULL,
-    signature      TEXT,
-    created_at     TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    id SERIAL PRIMARY KEY,
+    sender_id INT NOT NULL REFERENCES users(id),
+    recipient_id INT REFERENCES users(id),
+    group_id INT REFERENCES groups(id),
+    ciphertext TEXT NOT NULL,
+    encrypted_key TEXT NOT NULL,
+    nonce VARCHAR(24) NOT NULL,
+    auth_tag VARCHAR(24) NOT NULL,
+    signature TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 
     CONSTRAINT chk_recipient_xor_group CHECK (
         (recipient_id IS NOT NULL AND group_id IS NULL) OR
-        (recipient_id IS NULL     AND group_id IS NOT NULL)
+        (recipient_id IS NULL AND group_id IS NOT NULL)
     )
 );
 
-CREATE TABLE IF NOT EXISTS groups (
-    id                      SERIAL PRIMARY KEY,
-    name                    VARCHAR(100) NOT NULL,
-
-);
 
 CREATE TABLE IF NOT EXISTS group_members (
-    id_user                 INT REFERENCES users(id)
-    id_group                INT REFERENCES groups(id)
+    id_user INT REFERENCES users(id),
+    id_group INT REFERENCES groups(id)
 );
